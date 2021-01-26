219d98870a610785565683ca529537e56a89024c
Gean Neiva <gean_neiva@hotmail.com>
2021-01-26 15:27:14 -0300

    Add files via upload

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income'| 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
-  }
+   const {income, outcome} = this.transactions.reduce((accumulator,transaction) => {
+   switch(transaction.type){
+      case 'income':
+        (accumulator.income += transaction.value);
+        break;
+      case 'outcome':
+        (accumulator.outcome += transaction.value);
+        break;
 
-  public create(): Transaction {
-    // TODO
+      default:
+        break;
+   }
+      return accumulator;
+
+  },{
+     income:0,
+     outcome:0,
+     total:0
+   }
+   )
+   const total = income - outcome;
+   return {income, outcome,total}
+  };
+
+  
+  public create({ title, value, type}: CreateTransactionDTO):Transaction {
+    const transaction = new Transaction({ title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
